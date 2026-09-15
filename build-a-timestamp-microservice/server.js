f2848ce50beec5a2a7cd80b1b0c09a876d230492
Alex Williams <22619843+FlexWilliams@users.noreply.github.com>
2026-09-15 19:23:05 +0000

    ++timestamp microsvc

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,6 +13,37 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+
+app.get('/api', (req, res) => {
+  const date = new Date();
+
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get('/api/:date', (req, res) => {
+  console.log(`req.params.date: ${req.params.date}`);
+
+  const timestamp = Number(req.params.date);
+
+  const date = isNaN(timestamp) ? new Date(req.params.date ? req.params.date : null) : new Date(timestamp);
+
+  console.log(`date: ${date}`);
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  } else {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
+
+
+});
+
 // Do not change code below this line
 
 const PORT = 8000;
